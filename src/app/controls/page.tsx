20a64985d8fec38750d02e8690fafe13774b89f2
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-16 12:51:01 +0200

    feat: control page demo

--- src/app/controls/page.tsx
+++ src/app/controls/page.tsx
@@ -1,12 +1,34 @@
-import Link from 'next/link'
+'use client'
 
 export default () => {
+  const goToHome = () => {
+    console.log('go to home')
+  }
+  const goToDetail = () => {
+    console.log('go to detail')
+  }
+
   return (
-    <main className="p-10">
-      <h1 className="flex  p-4 pt-2 text-9xl">CONTROLS</h1>
-      <Link href="/" className="rounded-md bg-blue-500 p-4 text-white">
-        HOME
-      </Link>
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-8 text-4xl font-bold">Controls</h1>
+      <button
+        className="rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white hover:bg-blue-600"
+        onClick={() => goToHome()}
+      >
+        Ga terug
+      </button>
+      <button
+        className="rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white hover:bg-blue-600"
+        onClick={() => goToDetail()}
+      >
+        Lucht
+      </button>
+      <button
+        className="rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white hover:bg-blue-600"
+        onClick={() => goToDetail()}
+      >
+        Grond
+      </button>
     </main>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
-    <main className="p-10">
-      <h1 className="flex  p-4 pt-2 text-9xl">HELLO WORLD</h1>
-      <div className='flex gap-3'>
-        <Link href="/detail" className="rounded-md bg-blue-500 p-4 text-white">
-          DETAIL
-        </Link>
-        <Link href="/controls" className="rounded-md bg-blue-500 p-4 text-white">
-          CONTROLS
-        </Link>
-      </div>
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-8 text-4xl font-bold">Home Page</h1>
+     
     </main>
   )
 }
