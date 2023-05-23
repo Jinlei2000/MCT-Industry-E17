d5d2bf7a944ed5f281797dc6b1eb59221aa4f591
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-23 10:30:36 +0200

    feat: make a randomPics function

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -8,6 +8,27 @@ export default () => {
   const { getPhotoById, listenToChangeConfig } = useFireStore()
   const [photo, setPhoto] = useState<IPhoto>({})
   const [config, setConfig] = useState<IConfig>({})
+  const [generatedPics, setGeneratedPics] = useState<string[]>([])
+
+  // set 4 pics randomly of a tag from generatedPics
+  const randomPics = () => {
+    if (config.selectedTag !== '') {
+      let randomPics: string[] = []
+      const tag = config.selectedTag
+
+      const images =
+        tag && photo.generatedPics && photo.generatedPics[tag]
+          ? photo.generatedPics[tag]
+          : []
+
+      if (images.length > 4) {
+        randomPics = images.sort(() => 0.5 - Math.random()).slice(0, 4)
+      } else {
+        randomPics = images
+      }
+      setGeneratedPics(randomPics)
+    }
+  }
 
   useEffect(() => {
     listenToChangeConfig((config: IConfig) => {
@@ -18,43 +39,39 @@ export default () => {
   useEffect(() => {
     getPhotoById().then(photo => {
       setPhoto(photo)
+
+      // set 4 pics randomly of a tag from generatedPics when more than 4 pics
+      randomPics()
     })
   }, [config])
 
-  // get generated pic
-  const getGeneratedPic = (key: number) => {
-    const tag = config.selectedTag
-
-    return tag && photo.generatedPics && photo.generatedPics[tag][key]
-  }
-
   return (
     <main className="flex h-screen flex-col items-center justify-center p-10">
-      <h1 className="flex  p-4 pt-2 text-9xl">DETAIL</h1>
-
       {config?.selectedTag === '' ? (
-        <img className="object-cover" src={photo.url} alt={'Photo of'} />
+        // show original photo
+        <img className="object-cover" src={photo.url} alt={'Original photo'} />
       ) : (
+        // show 4 pics randomly of a tag from generatedPics
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
           <img
             className="object-cover"
-            src={getGeneratedPic(0)}
-            alt={'Photo of'}
+            src={generatedPics[0]}
+            alt={`AI generated photo of ${config.selectedTag}`}
           />
           <img
             className="object-cover"
-            src={getGeneratedPic(1)}
-            alt={'Photo of'}
+            src={generatedPics[1]}
+            alt={`AI generated photo of ${config.selectedTag}`}
           />
           <img
             className="object-cover"
-            src={getGeneratedPic(2)}
-            alt={'Photo of'}
+            src={generatedPics[2]}
+            alt={`AI generated photo of ${config.selectedTag}`}
           />
           <img
             className="object-cover"
-            src={getGeneratedPic(3)}
-            alt={'Photo of'}
+            src={generatedPics[3]}
+            alt={`AI generated photo of ${config.selectedTag}`}
           />
         </div>
       )}
