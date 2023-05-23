d3f124d40411f4dfc48e77d6a650e928ef420a19
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-23 16:41:31 +0200

    feat: download gefixed

--- src/app/download/page.tsx
+++
@@ -1,7 +0,0 @@
-export default () => {
-  return (
-    <main className="flex h-screen flex-col items-center justify-center p-10">
-      <h1 className="mb-8 text-4xl font-bold">Download</h1>
-    </main>
-  )
-}
