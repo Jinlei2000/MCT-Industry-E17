06d49376a0f9c81e450538f30dbdcb93887d3d35
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-23 19:18:27 +0200

    fix: merge conflicy

--- src/app/downloadImages/page.tsx
+++ src/app/downloadImages/page.tsx
@@ -1,11 +1,6 @@
 'use client'
 import useFireStore from '@/hooks/useFireStore'
-<<<<<<< HEAD
-import IPhoto from '@/interfaces/IPhoto'
-import { useEffect, useState } from 'react'
-=======
 import { getStorage, ref, getDownloadURL } from 'firebase/storage'
->>>>>>> 1.6.2
 import { saveAs } from 'file-saver'
 
 export default () => {
@@ -15,20 +10,6 @@ export default () => {
     <main className="flex h-screen flex-col items-center justify-center p-10">
       <h1 className="mb-8 text-4xl font-bold">Download</h1>
 
-<<<<<<< HEAD
-      <button
-        className="rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white hover:bg-blue-600"
-        onClick={() => {
-          console.log('download')
-          const url =
-            'https://firebasestorage.googleapis.com/v0/b/firestore-fa445.appspot.com/o/1%2F1-1_DIS.jpg?alt=media&token=9d74e8b7-9020-4241-8f16-69322a8797e4'
-
-          saveAs(url)
-        }}
-      >
-        sdqfqsdfqsdfsqdf
-      </button>
-=======
       <div className="flex flex-col gap-2">
         <button
           className="rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white hover:bg-blue-600"
@@ -65,7 +46,6 @@ export default () => {
           Download with firebase storage
         </button>
       </div>
->>>>>>> 1.6.2
     </main>
   )
 }
