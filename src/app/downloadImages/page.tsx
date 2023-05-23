771f3c7e7ca9e87e9b0b6e0693638b686a3719a4
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-23 18:37:02 +0200

    feat: test image download car

--- src/app/downloadImages/page.tsx
+++ src/app/downloadImages/page.tsx
@@ -25,11 +25,12 @@ export default () => {
         className="rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white hover:bg-blue-600"
         onClick={() => {
           console.log('download')
-          const url =
-            'https://firebasestorage.googleapis.com/v0/b/firestore-fa445.appspot.com/o/1%2F1-1_DIS.jpg?alt=media&token=9d74e8b7-9020-4241-8f16-69322a8797e4'
+          // const url =
+          //   'https://firebasestorage.googleapis.com/v0/b/firestore-fa445.appspot.com/o/1%2F1-1_DIS.jpg?alt=media&token=9d74e8b7-9020-4241-8f16-69322a8797e4'
 
-            saveAs(url)
-          
+          const url =
+            'https://images.pexels.com/photos/593172/pexels-photo-593172.jpeg'
+          saveAs(url)
         }}
       >
         sdqfqsdfqsdfsqdf
