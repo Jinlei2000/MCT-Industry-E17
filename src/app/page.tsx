fde745b80969ee38be191514c82321ec25692813
BlommeWout <wout.blomme@student.howest.be>
2023-05-25 11:17:41 +0200

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-8 text-9xl font-bold">Home Page</h1>
+      <h1 className="mb-8 text-9xl font-bold">hey</h1>
       <SideBar title='EXPO 50 JAAR E3/E17'/>
     </main>
   )
