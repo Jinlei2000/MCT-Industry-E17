735f6f95d1dc4ae0e887be4e04bf478538ad0de8
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-26 15:04:32 +0200

    feat: add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,36 +4,28 @@ import useFireStore from '@/hooks/useFireStore'
 import { useEffect } from 'react'
 
 export default function Home() {
-  const { listenToChangeConfig } = useFireStore()
+  // const { listenToChangeConfig } = useFireStore()
 
-  useEffect(() => {
-    listenToChangeConfig(() => {}, '/detail')
-  }, [])
+  // useEffect(() => {
+  //   listenToChangeConfig(() => {}, '/detail')
+  // }, [])
 
   return (
-    <main className="flex  h-screen flex-col items-center justify-end">
-      <div className="fixed z-0 h-screen w-screen">
-        {/* <iframe
-          src={
-            'https://www.youtube.com/embed/Q3bMHbJLhCE?autoplay=1&loop=1&controls=0&mute=1&playlist=Q3bMHbJLhCE'
-          }
-          allow="autoplay; loop; controls; mute;"
-          width={'100%'}
-          height={'100%'}
-          allowFullScreen
-        ></iframe> */}
+    <main className="relative overflow-hidden">
+      <video
+        src="/video.mov"
+        autoPlay
+        muted
+        loop
+        id="E17 highway"
+        className="h-screen w-screen object-cover"
+      />
 
-        <video
-          src="/video.mov"
-          autoPlay
-          muted
-          loop
-          id="E17 highway"
-          className="h-screen w-screen object-cover"
-        />
-      </div>
-      <div className="z-10 flex h-48 w-screen items-center justify-center bg-gradient-to-t from-black to-transparent text-4xl font-normal text-white">
-        Kijk in de toekomst van de E17 via de tablet
+      <div className="absolute bottom-0 left-0 z-10 flex w-screen justify-between bg-gradient-to-t from-black to-transparent">
+        <p className="py-8 pl-5 text-5xl font-semibold text-white">
+          Kijk in de toekomst van de E17 via de tablet
+        </p>
+        <div className="w-1/6"></div>
       </div>
 
       <SideBar title="EXPO 50 JAAR E3/E17" />
