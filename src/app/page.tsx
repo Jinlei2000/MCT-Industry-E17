e48217a2de6899a054054933f060d16d7e7ea1b4
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-05-26 11:22:53 +0200

    feat: add video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,41 @@
 'use client'
 import SideBar from '@/components/SideBar'
 import useFireStore from '@/hooks/useFireStore'
-import { useEffect, useRef } from 'react'
-import Youtube from 'react-youtube'
+import { useEffect } from 'react'
 
 export default function Home() {
   const { listenToChangeConfig } = useFireStore()
-  const videoRef = useRef(null)
-  const opts = {
-    
-    playerVars: {
-      autoplay: 1,
-    },
-  }
 
   useEffect(() => {
     listenToChangeConfig(() => {}, '/detail')
-    
-  
-  }, [videoRef])
+  }, [])
 
   return (
     <main className="flex  h-screen flex-col items-center justify-end">
-      <div className='fixed w-screen h-screen z-0'>
-        <iframe
-        
-          src={"https://www.youtube.com/embed/Q3bMHbJLhCE?autoplay=1&loop=1&controls=0&mute=1&playlist=Q3bMHbJLhCE"}
+      <div className="fixed z-0 h-screen w-screen">
+        {/* <iframe
+          src={
+            'https://www.youtube.com/embed/Q3bMHbJLhCE?autoplay=1&loop=1&controls=0&mute=1&playlist=Q3bMHbJLhCE'
+          }
           allow="autoplay; loop; controls; mute;"
-          width={"100%"}
-          height={"100%"}
-          
+          width={'100%'}
+          height={'100%'}
           allowFullScreen
-          
-        ></iframe>
+        ></iframe> */}
+
+        <video
+          src="/video.mov"
+          autoPlay
+          muted
+          loop
+          id="E17 highway"
+          className="h-screen w-screen object-cover"
+        />
       </div>
-      <div className='z-10 font-normal text-white text-4xl flex justify-center items-center w-screen h-48 bg-gradient-to-t from-black to-transparent'>
+      <div className="z-10 flex h-48 w-screen items-center justify-center bg-gradient-to-t from-black to-transparent text-4xl font-normal text-white">
         Kijk in de toekomst van de E17 via de tablet
       </div>
-      
+
       <SideBar title="EXPO 50 JAAR E3/E17" />
     </main>
   )
