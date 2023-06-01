69d04881acac2290dfc536c5f9e9507d217213c2
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-06-01 13:29:21 +0200

    feat: add ai img loader

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -64,7 +64,8 @@ export default () => {
     )
   }
 
-  const [isLoaded, setIsLoaded] = useState(true)
+  const [isLoadedImg, setIsLoadedImg] = useState(true)
+  const [isLoadedAiImgs, setIsLoadedAiImgs] = useState([true, true, true, true])
 
   return (
     <main className="relative h-screen">
@@ -73,7 +74,7 @@ export default () => {
         <>
           <SideBar title={customTitle} />
 
-          {isLoaded && (
+          {isLoadedImg && (
             // skeleton loader
             <div className="absolute z-[2] h-full w-full animate-pulse">
               <div className="grid h-full w-5/6 place-items-center bg-gray-400">
@@ -93,11 +94,11 @@ export default () => {
               alt={'Original photo'}
               fill
               quality={100}
-              onLoadingComplete={() => setIsLoaded(false)}
+              onLoadingComplete={() => setIsLoadedImg(false)}
             />
           )}
 
-          {!isLoaded && (
+          {!isLoadedImg && (
             // show text when image is loaded
             <div className="fixed bottom-0 left-0 m-12 max-w-md border-2 border-white/100 bg-black/40 p-2 ">
               <div className="font text-3xl text-white opacity-100">
@@ -110,18 +111,56 @@ export default () => {
         // show 4 pics randomly of a tag from generatedPics
         <>
           <SideBar title={AiTitle} />
+
+          {/* skeleton loader */}
+          <div className="absolute z-[2] h-screen w-5/6">
+            <div className="grid h-full grid-cols-2 grid-rows-2">
+              {isLoadedAiImgs.map((isLoadedAiImg, index) => (
+                <div key={index}>
+                  {isLoadedAiImg && (
+                    <div
+                      key={`${index}-skeleton`}
+                      className="grid h-full w-full animate-pulse place-items-center border bg-gray-400"
+                    >
+                      <svg
+                        key={`${index}-svg`}
+                        className="h-20 w-20 fill-gray-200"
+                        viewBox="0 0 640 512"
+                      >
+                        <path
+                          key={`${index}-path`}
+                          d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"
+                        />
+                      </svg>
+                      <span key={`${index}-span`} className="sr-only">
+                        Loading...
+                      </span>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+
           {generatedPics.length > 0 && (
             <div className="h-screen w-5/6">
               <div className="grid h-full grid-cols-2 grid-rows-2">
                 {generatedPics.map((picUrl, index) => (
                   <div key={index} className="relative">
                     <Image
-                      key={index}
+                      key={`${index}-image`}
                       className="object-cover"
                       src={picUrl}
                       alt={`AI generated photo of ${config.selectedTag}`}
                       fill={true}
                       quality={100}
+                      onLoadingComplete={() => {
+                        setIsLoadedAiImgs(prev => {
+                          const newIsLoadedAiImgs = [...prev]
+                          newIsLoadedAiImgs[index] = false
+                          return newIsLoadedAiImgs
+                        })
+                      }}
                     />
                   </div>
                 ))}
