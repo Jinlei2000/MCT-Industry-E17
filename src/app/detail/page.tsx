9fa801d8c2f874d1360ebf808b0852ae114e5b28
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-06-01 13:44:07 +0200

    feat: imgSkelenton component

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import SideBar from '@/components/SideBar'
+import ImgSkeleton from '@/components/imgSkeleton'
 import useFireStore from '@/hooks/useFireStore'
 import IConfig from '@/interfaces/IConfig'
 import IPhoto from '@/interfaces/IPhoto'
@@ -69,25 +70,18 @@ export default () => {
 
   return (
     <main className="relative h-screen">
+      {/* original photo or ai generated photos */}
       {config.selectedTag === '' ? (
-        // show original photo
         <>
           <SideBar title={customTitle} />
 
+          {/* skeleton loader */}
           {isLoadedImg && (
-            // skeleton loader
-            <div className="absolute z-[2] h-full w-full animate-pulse">
-              <div className="grid h-full w-5/6 place-items-center bg-gray-400">
-                <svg className="h-20 w-20 fill-gray-200" viewBox="0 0 640 512">
-                  <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-                </svg>
-              </div>
-              <span className="sr-only">Loading...</span>
-            </div>
+            <ImgSkeleton className="absolute z-[2] h-full w-5/6" />
           )}
 
+          {/* show original photo */}
           {photo.url && (
-            // show original photo
             <Image
               className="object-cover"
               src={photo.url}
@@ -98,8 +92,8 @@ export default () => {
             />
           )}
 
+          {/* show description */}
           {!isLoadedImg && (
-            // show text when image is loaded
             <div className="fixed bottom-0 left-0 m-12 max-w-md border-2 border-white/100 bg-black/40 p-2 ">
               <div className="font text-3xl text-white opacity-100">
                 De Europese weg 17 is ongeveer 696 kilometer lang.
@@ -108,7 +102,6 @@ export default () => {
           )}
         </>
       ) : (
-        // show 4 pics randomly of a tag from generatedPics
         <>
           <SideBar title={AiTitle} />
 
@@ -117,31 +110,13 @@ export default () => {
             <div className="grid h-full grid-cols-2 grid-rows-2">
               {isLoadedAiImgs.map((isLoadedAiImg, index) => (
                 <div key={index}>
-                  {isLoadedAiImg && (
-                    <div
-                      key={`${index}-skeleton`}
-                      className="grid h-full w-full animate-pulse place-items-center border bg-gray-400"
-                    >
-                      <svg
-                        key={`${index}-svg`}
-                        className="h-20 w-20 fill-gray-200"
-                        viewBox="0 0 640 512"
-                      >
-                        <path
-                          key={`${index}-path`}
-                          d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"
-                        />
-                      </svg>
-                      <span key={`${index}-span`} className="sr-only">
-                        Loading...
-                      </span>
-                    </div>
-                  )}
+                  {isLoadedAiImg && <ImgSkeleton className="h-full w-full" />}
                 </div>
               ))}
             </div>
           </div>
 
+          {/* show 4 pics randomly of a tag from generated ai pics */}
           {generatedPics.length > 0 && (
             <div className="h-screen w-5/6">
               <div className="grid h-full grid-cols-2 grid-rows-2">
