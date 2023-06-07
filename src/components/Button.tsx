eb3beff55d7a9bea02f1fc7d78480c5a7a18016d
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-06-07 13:45:32 +0200

    feat: download page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export default ({
 }) => {
   return (
     <button
-      className="h-20  bg-e17-secondary-700 text-2xl font-bold text-white xl:h-24  xl:text-3xl"
+      className="h-20 bg-e17-secondary-700 text-2xl font-bold text-white xl:h-24 xl:text-3xl"
       onClick={handleClick}
     >
       {children}
