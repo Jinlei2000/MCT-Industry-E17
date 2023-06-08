271145d776c2fe163af9a9d77883f68ef3f96b05
Jinlei2000 <91153798+Jinlei2000@users.noreply.github.com>
2023-06-08 11:02:32 +0200

    feat: setup next config next-image-export-optimizer

--- next.config.js
+++ next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [320, 420, 768, 1024, 1200, 1440, 1920, 2048, 3840, 4096],
   },
+  transpilePackages: ['next-image-export-optimizer'],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: 'public',
+    nextImageExportOptimizer_exportFolderPath: 'out',
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
+
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="empty"` to all <ExportedImage> components.
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
 }
 
 module.exports = nextConfig
