bb7ea455093a17adac11dbae36e082584760536a
BlommeWout <wout.blomme@student.howest.be>
2023-06-19 14:06:09 +0200

    changed watermark

--- src/hooks/useWaterMark.tsx
+++ src/hooks/useWaterMark.tsx
@@ -9,7 +9,7 @@ export default () => {
       body: JSON.stringify({
         mainImageUrl: url,
         markImageUrl:
-          'https://firebasestorage.googleapis.com/v0/b/jaar-e17-ai.appspot.com/o/logo%2Flogo_leiedal.png?alt=media',
+          'https://firebasestorage.googleapis.com/v0/b/jaar-e17-ai.appspot.com/o/logo%2F50_jaar_E17_-_Logo_CMYK.jpg?alt=media',
         markRatio: 0.15,
         margin: 0,
       }),
